bd22862dab8bacb6c139fc36ed82dcfb5cbd1b03
Yanrui Wang <yw2226@cornell.edu>
2020-05-20 20:43:09 +0800

    Update 0021Valid_Parentheses1.py

--- problems/0021Valid_Parentheses/0021Valid_Parentheses1.py
+++ problems/0021Valid_Parentheses/0021Valid_Parentheses1.py
@@ -1,6 +1,5 @@
 """
-leetcode 0001 TwoSum
-My submission
+Valid Parentheses
 
 Version: 1.1 
 Author:  Yanrui 
@@ -9,16 +8,37 @@ date: 	 5/20/2020
 
 
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in nums:
-                if i != nums.index(diff):
-                    return [i, nums.index(diff)]
+    def isValid(self, s):
+      if len(s):
+          parenthese_left = ['(', '{', '[']
+          parenthese_right = [')', '}', ']']
+          num_record = []
+          for i in range(len(s)):
+              if s[i] in parenthese_left:
+                  # print('position 1')
+                  # print(parenthese_left.index(s[i]))
+                  num_record.append(parenthese_left.index(s[i]))
+              else:
+                  # print('position 2')
+                  if not len(num_record):
+                      return False
+                  elif(parenthese_right.index(s[i]) == num_record[len(num_record) - 1]):
+                      # print('ready to pop')
+                      # print('num_record:', num_record)
+                      num_record.pop()
+                      # print('after pop')
+                      # print('num_record:', num_record)
+                  else:
+                      return False
+          if not len(num_record):
+              return True
+          else:
+              return False
+      else:
+          return True
 
 
 if __name__ == '__main__':
-	s = Solution()
-	nums = [2, 7, 11, 15]
-	target = 9
-	print(s.twoSum(nums, target))
+    a = Solution()
+    input_string = "({[]})"
+    print(a.isValid(input_string))
