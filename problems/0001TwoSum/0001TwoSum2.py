97d20a3961276c93b6e8691774d21817b6e4e399
Yanrui Wang <yw2226@cornell.edu>
2020-05-20 19:19:42 +0800

    add solution file of TwoSum

---
+++ problems/0001TwoSum/0001TwoSum2.py
@@ -0,0 +1,24 @@
+"""
+leetcode 0001 TwoSum
+My submission
+
+Version: 1.1 
+Author:  Yanrui 
+date: 	 5/20/2020
+"""
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in nums:
+                if i != nums.index(diff):
+                    return [i, nums.index(diff)]
+
+
+if __name__ == '__main__':
+	s = Solution()
+	nums = [2, 7, 11, 15]
+	target = 9
+	print(s.twoSum(nums, target))
