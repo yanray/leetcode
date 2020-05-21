de840901bc9bb273932d97aea99c63ad09b4bc08
Yanrui Wang <yw2226@cornell.edu>
2020-05-21 13:34:24 +0800

    Update 0021Merge_Two_Sorted _Lists2.py

--- problems/0021Merge_Two_Sorted _Lists/0021Merge_Two_Sorted _Lists2.py
+++ problems/0021Merge_Two_Sorted _Lists/0021Merge_Two_Sorted _Lists2.py
@@ -42,24 +42,16 @@ class SingleLinkedList:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = ListNode(None)
-        node = dummy
-        
-        while l1 and l2: 
-            if l1.val < l2.val:
-                node.next = l1
-                l1= l1.next
-            else: 
-                node.next = l2
-                l2 = l2.next
-            node = node.next
-                
-        if l1:
-            node.next = l1
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
         else:
-            node.next = l2
-            
-        return dummy.next
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
 
 if __name__ == '__main__':
