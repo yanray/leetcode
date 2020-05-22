1037ac5333aec278d06051c5218fa07a97eb4f72
Yanrui Wang <yw2226@cornell.edu>
2020-05-22 20:04:16 +0800

    Create 0121Best_Time_to_Buy_and_Sell_Stock1.py

---
+++ problems/0121Best_Time_to_Buy_and_Sell_Stock/0121Best_Time_to_Buy_and_Sell_Stock1.py
@@ -0,0 +1,19 @@
+"""
+if else 
+
+Version: 1.1 
+Author:  Yanrui 
+date:      5/22/2020
+"""
+
+
+class Solution:
+    def reorderLogFiles(self, logs):
+
+
+
+
+if __name__ == '__main__':
+  a = Solution()
+
+
