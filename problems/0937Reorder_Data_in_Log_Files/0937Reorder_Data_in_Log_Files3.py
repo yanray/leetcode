9e93750743e2b07a321e00776a7d76becb2a5dd1
Yanrui Wang <yw2226@cornell.edu>
2020-05-22 19:43:19 +0800

    Create 0937Reorder_Data_in_Log_Files3.py

---
+++ problems/0937Reorder_Data_in_Log_Files/0937Reorder_Data_in_Log_Files3.py
@@ -0,0 +1,23 @@
+"""
+if else 
+
+Version: 1.1 
+Author:  Yanrui 
+date:      5/22/2020
+"""
+
+
+class Solution:
+    def reorderLogFiles(self, logs):
+
+
+
+if __name__ == '__main__':
+  a = Solution()
+
+  # logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+  logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a2 act car"]
+
+  print("Output: ", a.reorderLogFiles(logs))
+
+
