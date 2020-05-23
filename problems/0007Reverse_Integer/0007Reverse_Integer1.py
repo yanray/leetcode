8fa67cd801782b80468711279c05e6d4cbb9dfc9
Yanrui Wang <yw2226@cornell.edu>
2020-05-24 00:35:03 +0800

    Update 0007Reverse_Integer1.py

--- problems/0007Reverse_Integer/0007Reverse_Integer1.py
+++ problems/0007Reverse_Integer/0007Reverse_Integer1.py
@@ -8,16 +8,31 @@ date: 	 5/20/2020
 
 
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in nums:
-                if i != nums.index(diff):
-                    return [i, nums.index(diff)]
+    def reverse(self, x):
+        
+        lower_limit = -pow(2, 31)
+        upper_limit = pow(2, 31) - 1
+        
+        rev_x = 0
+        if x < 0:
+            sign = -1
+            x = -x
+        else: 
+            sign = 1
+
+        while x != 0:
+            x, temp = divmod(x, 10)
+            rev_x = rev_x * 10 + temp
+        rev_x = rev_x * sign
+            
+        return rev_x if rev_x < upper_limit and rev_x > lower_limit else 0 
 
 
 if __name__ == '__main__':
-	s = Solution()
-	nums = [2, 7, 11, 15]
-	target = 9
-	print(s.twoSum(nums, target))
+    a = Solution()
+
+    num = 123
+    print('Input: ', num)
+    print('Output:', a.reverse(num))
+
+
