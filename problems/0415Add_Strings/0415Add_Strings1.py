abd3e60dac7328497e0d56f667049886e8527331
Yanrui Wang <yw2226@cornell.edu>
2020-05-24 01:46:45 +0800

    Update 0415Add_Strings1.py

--- problems/0415Add_Strings/0415Add_Strings1.py
+++ problems/0415Add_Strings/0415Add_Strings1.py
@@ -9,21 +9,27 @@ date:      5/23/2020
 
 class Solution:
     def addStrings(self, num1, num2):
-        right_order = 'abcdefghijklmnopqrstuvwxyz'
-        
-        trans = str.maketrans(order, right_order)
-        new_words = [w.translate(trans) for w in words]
-        
-        for i in range(len(new_words) - 1): 
-            if new_words[i] > new_words[i + 1]:
-                return False
-        
-        return True
+        int_num1 = 0
+        for i in range(len(num1)):
+            temp = ord(num1[i]) - 48
+            int_num1 = int_num1 * 10 + temp
+
+        int_num2 = 0
+        for i in range(len(num2)):
+            temp = ord(num2[i]) - 48
+            int_num2 = int_num2 * 10 + temp
+            
+        return str(int_num1 + int_num2)
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    print(a.isAlienSorted(words, order))
+    num1 = '1325'
+    num2 = '25219'
+
+    print('num1: ', num1)
+    print('num2: ', num2)
+    print('num1 + num2:', a.addStrings(num1, num2))
 
 
