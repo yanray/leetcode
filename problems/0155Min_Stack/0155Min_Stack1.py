bdc6f9f2859b8d761ee234fc720fe4656c381377
Yanrui Wang <yw2226@cornell.edu>
2020-05-26 00:20:03 +0800

    Create 0155Min_Stack1.py

---
+++ problems/0155Min_Stack/0155Min_Stack1.py
@@ -0,0 +1,47 @@
+"""
+if else 
+
+Version: 1.1 
+Author:  Yanrui 
+date:      5/25/2020
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.Stack = []
+
+    def push(self, x: int) -> None:
+        self.Stack.append(x)
+
+    def pop(self) -> None:
+        if self.Stack:
+            self.Stack.pop()
+
+    def top(self) -> int:
+        if self.Stack:
+            return self.Stack[-1]
+
+    def getMin(self) -> int:
+        temp = self.Stack
+        
+        if self.Stack:
+            return sorted(temp)[0]
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    words = ["hello","leetcode"]
+
+    order = "hlabcdefgijkmnopqrstuvwxyz"
+
+    print(a.isAlienSorted(words, order))
+
+
