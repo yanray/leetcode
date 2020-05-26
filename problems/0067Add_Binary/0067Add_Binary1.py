981bd8f5d92252c6b9825b5c8b2094e3866e58e9
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 01:36:08 +0800

    Update 0067Add_Binary1.py

--- problems/0067Add_Binary/0067Add_Binary1.py
+++ problems/0067Add_Binary/0067Add_Binary1.py
@@ -8,12 +8,27 @@ date: 	 5/20/2020
 
 
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
-      return x >= 0 and x == int(str(x)[::-1])
-      # return x >= 0 and x == int(f"{x}"[::-1])
+    def addBinary(self, a: str, b: str) -> str:
+        
+        int_a = 0
+        int_b = 0
+        
+        digit = 0
+        for bit in a[::-1]:
+            int_a += 2 ** digit * int(bit)
+            digit += 1
+        digit = 0
+        for bit in b[::-1]:
+            int_b += 2 ** digit * int(bit)
+            digit += 1
+        
+        return str(bin(int_a + int_b))[2:]
 
 
 if __name__ == '__main__':
-    a = Solution()
+    s = Solution()
 
-    print(a.isPalindrome(121))
+    a = "1010"
+    b = "1011"
+
+    print(s.addBinary(a, b))
