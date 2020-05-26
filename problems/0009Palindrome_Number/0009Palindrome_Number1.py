dd8fca36f6c684d4eac835a83afd2e404c01a88b
Yanrui Wang <yw2226@cornell.edu>
2020-05-26 19:16:11 +0800

    Create 0009Palindrome_Number1.py

---
+++ problems/0009Palindrome_Number/0009Palindrome_Number1.py
@@ -0,0 +1,38 @@
+"""
+leetcode 0001 TwoSum
+
+Version: 1.1 
+Author:  Yanrui 
+date: 	 5/26/2020
+"""
+
+
+class Solution:
+    def reverse(self, x):
+        
+        lower_limit = -pow(2, 31)
+        upper_limit = pow(2, 31) - 1
+        
+        rev_x = 0
+        if x < 0:
+            sign = -1
+            x = -x
+        else: 
+            sign = 1
+
+        while x != 0:
+            x, temp = divmod(x, 10)
+            rev_x = rev_x * 10 + temp
+        rev_x = rev_x * sign
+            
+        return rev_x if rev_x < upper_limit and rev_x > lower_limit else 0 
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    num = 123
+    print('Input: ', num)
+    print('Output:', a.reverse(num))
+
+
