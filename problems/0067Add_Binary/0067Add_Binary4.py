3b3b4155595695abf7cee29756e4224825846d09
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 02:18:53 +0800

    Create 0067Add_Binary4.py

---
+++ problems/0067Add_Binary/0067Add_Binary4.py
@@ -0,0 +1,28 @@
+"""
+Valid Parentheses
+
+Version: 1.1 
+Author:  Yanrui 
+date: 	 5/20/2020
+"""
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        x, y = int(a, 2), int(b, 2)
+
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    a = "1010"
+    b = "1011"
+
+    print(s.addBinary(a, b))
