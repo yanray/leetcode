ed8b10a9ae0a4977c4019d1c2ffdaf1d07dd0fd8
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 14:07:16 +0800

    Update 0680Valid_Palindrome_II1.py

--- problems/0680Valid_Palindrome_II/0680Valid_Palindrome_II1.py
+++ problems/0680Valid_Palindrome_II/0680Valid_Palindrome_II1.py
@@ -1,33 +1,32 @@
 """
-if else 
 
 Version: 1.1 
 Author:  Yanrui 
-date:      5/23/2020
+date:    5/27/2020
 """
 
-
 class Solution:
-    def isAlienSorted(self, words, order):
-        right_order = 'abcdefghijklmnopqrstuvwxyz'
-        
-        trans = str.maketrans(order, right_order)
-        new_words = [w.translate(trans) for w in words]
-        
-        for i in range(len(new_words) - 1): 
-            if new_words[i] > new_words[i + 1]:
-                return False
-        
-        return True
+    def validPalindrome(self, s: str) -> bool:
 
+        def isPalindrome(s):
+            return s == s[::-1]
+        
+        if s == s[::-1]:
+            return True
+        
+        first, last = 0, len(s) - 1
+        
+        while first < last:
+            if s[first] != s[last]:
+                return isPalindrome(s[first + 1 : last + 1]) or isPalindrome(s[first : last])
+            first += 1
+            last -= 1
+            
+        return True
 
 if __name__ == '__main__':
     a = Solution()
 
-    words = ["hello","leetcode"]
-
-    order = "hlabcdefgijkmnopqrstuvwxyz"
-
-    print(a.isAlienSorted(words, order))
-
+    s = "eedede"
+    print(a.validPalindrome(s))
 
