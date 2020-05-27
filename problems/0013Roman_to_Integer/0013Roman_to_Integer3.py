f74ae99bd738bd2443fbb9b46147b272cf8f5e2a
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 14:49:12 +0800

    Create 0013Roman_to_Integer3.py

---
+++ problems/0013Roman_to_Integer/0013Roman_to_Integer3.py
@@ -0,0 +1,29 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date:    5/27/2020
+"""
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        roman_dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        
+        sum_int = 0
+        i = 0
+        for i in range(len(s) - 1):
+            if roman_dict[s[i]] < roman_dict[s[i + 1]]:
+                sum_int -= roman_dict[s[i]]
+            else:
+                sum_int += roman_dict[s[i]]
+        sum_int += roman_dict[s[-1]]
+        
+        return sum_int
+
+if __name__ == '__main__':
+    a = Solution()
+
+    s = "MCMXCIV"
+    print(a.romanToInt(s))
+
