dca9f3ad8a91eb52ee9b14c2a7c364e068c7cd92
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 16:21:31 +0800

    Create 0136Single_Number1.py

---
+++ problems/0136Single_Number/0136Single_Number1.py
@@ -0,0 +1,32 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date:    5/27/2020
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        ways = 0
+        first = 1
+        second = 2
+        
+        if n == 1:
+            return first
+        elif n == 2:
+            return second
+        else:
+            for i in range(2, n):
+                ways = first + second
+                first = second
+                second = ways
+
+        return ways
+
+if __name__ == '__main__':
+    a = Solution()
+
+    n = 10
+    print(a.climbStairs(n))
+
