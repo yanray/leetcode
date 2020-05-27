03bfb92b0a67023bcb38fee2bdbaff432562a9c9
Yanrui Wang <yw2226@cornell.edu>
2020-05-27 22:05:01 +0800

    Update 0014Longest_Common_Prefix1.py

--- problems/0014Longest_Common_Prefix/0014Longest_Common_Prefix1.py
+++ problems/0014Longest_Common_Prefix/0014Longest_Common_Prefix1.py
@@ -6,13 +6,32 @@ date:    5/27/2020
 """
 
 class Solution:
-    def singleNumber(self, nums) -> int:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        if not strs:
+            return ""
+        else:
+            prefix = strs[0]
+            for s in strs:
+                temp = prefix
+                if temp == "":
+                    return prefix
+                else:
+                    prefix = ""
+                    for i in range(min(len(temp), len(s))):
+                        if temp[i] == s[i]:
+                            prefix += s[i]
+                        else:
+                            break
 
-        return sum(set(nums)) * 2 - sum(nums)
+            return prefix
+
 
 if __name__ == '__main__':
     a = Solution()
 
-    nums = [4,1,2,1,2]
-    print(a.singleNumber(nums))
+    # strs = ["flower","flow","flight"]
+    # strs = ["dog","racecar","car"]
+    strs = []
+    print(a.longestCommonPrefix(strs))
 
