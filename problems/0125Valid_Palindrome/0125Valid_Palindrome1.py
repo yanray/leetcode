5a2bc9785b43ab1981e08bb238cc06dd988ad1fb
Yanrui Wang <yw2226@cornell.edu>
2020-05-28 23:06:53 +0800

    Update 0125Valid_Palindrome1.py

--- problems/0125Valid_Palindrome/0125Valid_Palindrome1.py
+++ problems/0125Valid_Palindrome/0125Valid_Palindrome1.py
@@ -2,36 +2,30 @@
 
 Version: 1.1 
 Author:  Yanrui 
-date:    5/27/2020
+date:    5/28/2020
 """
 
 class Solution:
-    def longestCommonPrefix(self, strs) -> str:
+    def isPalindrome(self, s: str) -> bool:
         
-        if not strs:
-            return ""
-        else:
-            prefix = strs[0]
-            for s in strs:
-                temp = prefix
-                if temp == "":
-                    return prefix
-                else:
-                    prefix = ""
-                    for i in range(min(len(temp), len(s))):
-                        if temp[i] == s[i]:
-                            prefix += s[i]
-                        else:
-                            break
-
-            return prefix
+        if not s:
+            return True
+        
+        num_al = []
+        for ch in s:
+            if ch.isdigit():
+                num_al.append(ch)
+            elif ch.islower():
+                num_al.append(ch)
+            elif ch.isupper():
+                num_al.append(ch.lower())
+                
+        return num_al == num_al[::-1]
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    strs = ["flower","flow","flight"]
-    # strs = ["dog","racecar","car"]
-    # strs = []
-    print(a.longestCommonPrefix(strs))
+    s = "A man, a plan, a canal: Panama"
+    print(a.isPalindrome(s))
 
