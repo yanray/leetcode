ce561144d52da456489359da74fb28a4222051ae
Yanrui Wang <yw2226@cornell.edu>
2020-05-28 16:53:17 +0800

    Create 0125Valid_Palindrome1.py

---
+++ problems/0125Valid_Palindrome/0125Valid_Palindrome1.py
@@ -0,0 +1,37 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date:    5/27/2020
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        
+        if not strs:
+            return ""
+        else:
+            prefix = strs[0]
+            for s in strs:
+                temp = prefix
+                if temp == "":
+                    return prefix
+                else:
+                    prefix = ""
+                    for i in range(min(len(temp), len(s))):
+                        if temp[i] == s[i]:
+                            prefix += s[i]
+                        else:
+                            break
+
+            return prefix
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    strs = ["flower","flow","flight"]
+    # strs = ["dog","racecar","car"]
+    # strs = []
+    print(a.longestCommonPrefix(strs))
+
