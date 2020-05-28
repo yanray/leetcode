d5c5ed6b946adedd11a42ac8750521182e68095d
Yanrui Wang <yw2226@cornell.edu>
2020-05-28 15:47:45 +0800

    Create 0014Longest_Common_Prefix2.py

---
+++ problems/0014Longest_Common_Prefix/0014Longest_Common_Prefix2.py
@@ -0,0 +1,29 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date:    5/27/2020
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        
+        if not strs:
+            return ""
+        else:
+            prefix = strs[0][0]
+            for j in range(len())
+            for i in range(len(strs)):
+                if prefix == strs[i][0]
+
+            return prefix
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    strs = ["flower","flow","flight"]
+    # strs = ["dog","racecar","car"]
+    # strs = []
+    print(a.longestCommonPrefix(strs))
+
