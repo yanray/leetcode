c1e9482e18d3917bfff701d394c3a247e575b4cd
Yanrui Wang <yw2226@cornell.edu>
2020-05-29 23:22:58 +0800

    Update 0038Count_and_Say1.py

--- problems/0038Count_and_Say/0038Count_and_Say1.py
+++ problems/0038Count_and_Say/0038Count_and_Say1.py
@@ -6,28 +6,30 @@ date:    5/28/2020
 """
 
 class Solution:
-    def countPrimes(self, n: int) -> int:
+    def countAndSay(self, n: int) -> str:
+        
+        cs_sequence = ["0", "1"]
+        
+        for i in range(1, n):
+            prev_cs_str = cs_sequence[i]
+            
+            cs_str = ""
+            num_count = 0
+            for j, ch in enumerate(prev_cs_str):
+                if (j + 1) == len(prev_cs_str) or prev_cs_str[j + 1] != ch:
+                    cs_str += str(num_count + 1) + ch
+                    num_count = 0
+                elif prev_cs_str[j + 1] == ch:
+                    num_count += 1
 
-        num_list = [1] * n
-        num_list[0] = 0
-        num_list[1] = 0
-        num_list[2] = 1
-
-        for i in range(1, int(n ** 0.5) + 1):
-            if num_list[i] == 0:
-                continue
-            else:
-                num_list[i] = 1
-                for j in range(i * i, n, i):
-                    num_list[j] = 0
-
-        print(num_list)
-        return sum(num_list)
+            cs_sequence.append(cs_str)
+            
+        return cs_sequence[n]
 
 if __name__ == '__main__':
     a = Solution()
 
 
-    n = 10
-    print(a.countPrimes(n))
+    n = 5
+    print(a.countAndSay(n))
 
