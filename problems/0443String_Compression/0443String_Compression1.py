30750bc79beb5a167a1082aae4d0bbbd8802a43d
Yanrui Wang <yw2226@cornell.edu>
2020-05-29 14:16:36 +0800

    Update 0443String_Compression1.py

--- problems/0443String_Compression/0443String_Compression1.py
+++ problems/0443String_Compression/0443String_Compression1.py
@@ -4,28 +4,28 @@ Version: 1.1
 Author:  Yanrui 
 date:    5/28/2020
 """
+from typing import List
+import collections
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        
-        if not s:
-            return True
-        
-        num_al = []
-        for ch in s:
-            if ch.isdigit():
-                num_al.append(ch)
-            elif ch.islower():
-                num_al.append(ch)
-            elif ch.isupper():
-                num_al.append(ch.lower())
-                
-        return num_al == num_al[::-1]
+    def compress(self, chars: List[str]) -> int:
+
+        anchor = write = 0
+        for read, c in enumerate(chars):
+            if read + 1 == len(chars) or chars[read + 1] != c:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    s = "A man, a plan, a canal: Panama"
-    print(a.isPalindrome(s))
+    chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    print(a.compress(chars))
 
