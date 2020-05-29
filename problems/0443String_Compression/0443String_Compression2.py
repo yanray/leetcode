38f26b2c5382c341304c7e73bee531c1d5152f3a
Yanrui Wang <yw2226@cornell.edu>
2020-05-29 14:46:45 +0800

    Update 0443String_Compression2.py

--- problems/0443String_Compression/0443String_Compression2.py
+++ problems/0443String_Compression/0443String_Compression2.py
@@ -21,12 +21,19 @@ class Solution:
         pointer = 0
         for i in range(len(chars) - 1):
             if (i + 2) == len(chars) and chars[i + 1] == chars[pointer]:
-                chars[pointer + 1] = str(count_ch + 1)
-                pointer += 2
+                pointer += 1
+                for digit in str(count_ch + 1):
+                    chars[pointer] = digit
+                    pointer += 1
             elif (i + 2) == len(chars) and chars[i + 1] != chars[pointer]:
-                chars[pointer + 1] = str(count_ch)
-                chars[pointer + 2] = chars[i + 1]
-                pointer += 3
+                pointer += 1
+                if count_ch != 1:
+                    chars[pointer] = str(count_ch)
+                    chars[pointer + 1] = chars[i + 1]
+                    pointer += 2
+                else:
+                    chars[pointer] = chars[i + 1]
+                    pointer += 1
             elif chars[i] == chars[pointer] and chars[i + 1] == chars[pointer]:
                 count_ch += 1
             elif chars[i] == chars[pointer] and chars[i + 1] != chars[pointer]:
@@ -34,19 +41,23 @@ class Solution:
                     chars[pointer + 1] = chars[i + 1]
                     pointer += 1
                 else:
-                    chars[pointer + 1] = str(count_ch)
-                    chars[pointer + 2] = chars[i + 1]
-                    pointer += 2
+                    pointer += 1
+                    for digit in str(count_ch):
+                        chars[pointer] = digit
+                        pointer += 1
+                    chars[pointer] = chars[i + 1]
                     count_ch = 1
 
 
-        return chars[:pointer]
+        return len(chars[:pointer])
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    chars = ["a","a","b","b","c","c", "c", "d", "d"]
-    chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    # chars = ["a","a","b","c","c", "c", "d"]
+    # chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+    chars = ["a", "b", "c", "c", "d"]
+    chars = ["a","b","c","d","e","f","g","g","g","g","g","g","g","g","g","g","g","g","a","b","c"]
     print(a.compress(chars))
 
