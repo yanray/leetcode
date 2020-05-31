3f30d3dab772879a2715479cbecdf5eeaee5df10
Yanrui Wang <yw2226@cornell.edu>
2020-05-31 16:25:57 +0800

    Update 0359Logger_Rate_Limiter1.py

--- problems/0359Logger_Rate_Limiter/0359Logger_Rate_Limiter1.py
+++ problems/0359Logger_Rate_Limiter/0359Logger_Rate_Limiter1.py
@@ -5,29 +5,41 @@ Author:  Yanrui
 date:    5/30/2020
 """
 
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        
-        see_once = {}
-        for i, ch in enumerate(s):
-            if ch in see_once:
-                see_once[ch] = -1
-            else:
-                see_once[ch] = i
+class Logger:
 
-        for v in see_once.values():
-            if v != -1:
-                return v
-                
-        return -1
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.log_dict = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
+        if message not in self.log_dict:
+            self.log_dict[message] = timestamp
+            return True
+        else:
+            if timestamp - self.log_dict[message] >= 10:
+                self.log_dict[message] = timestamp
+                return True
+            else:
+                return False
 
 
 if __name__ == '__main__':
-    a = Solution()
 
-    s = "leetcode"
+    logger = Logger()
 
-    print(a.firstUniqChar(s))
+    print(logger.shouldPrintMessage(1, "foo"))
+    print(logger.shouldPrintMessage(2, "bar"))
+    print(logger.shouldPrintMessage(3, "foo"))
+    print(logger.shouldPrintMessage(8, "bar"))
+    print(logger.shouldPrintMessage(10, "foo"))
+    print(logger.shouldPrintMessage(11, "foo"))
 
 
     
