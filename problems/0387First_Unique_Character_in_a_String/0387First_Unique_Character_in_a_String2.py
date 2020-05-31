bbefe97975712397ec413b1bc5b10aa344ec2c1f
Yanrui Wang <yw2226@cornell.edu>
2020-05-31 14:35:50 +0800

    Create 0387First_Unique_Character_in_a_String2.py

---
+++ problems/0387First_Unique_Character_in_a_String/0387First_Unique_Character_in_a_String2.py
@@ -0,0 +1,34 @@
+"""
+Reverse Linked List
+
+Version: 1.1 
+Author:  Yanrui 
+date:    5/30/2020
+"""
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map : character and how often it appears
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
+        return -1
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    s = "leetcode"
+
+    print(a.firstUniqChar(s))
+
+
+    
