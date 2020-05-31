4ba3c9c8de67e86626713755c91e3ea1063b7213
Yanrui Wang <yw2226@cornell.edu>
2020-05-31 14:32:39 +0800

    Update 0387First_Unique_Character_in_a_String1.py

--- problems/0387First_Unique_Character_in_a_String/0387First_Unique_Character_in_a_String1.py
+++ problems/0387First_Unique_Character_in_a_String/0387First_Unique_Character_in_a_String1.py
@@ -6,34 +6,29 @@ Author:  Yanrui
 date:    5/30/2020
 """
 
-from typing import List
-import string
-import collections
-
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+    def firstUniqChar(self, s: str) -> int:
         
-        old_punc = string.punctuation
-        new_punc = ' ' * len(string.punctuation)
-        words = paragraph.translate(str.maketrans(old_punc, new_punc)).lower().split()
-
-        words_dict = collections.Counter(words)
-
-        for i in range(len(words_dict)):
-            temp = words_dict.most_common(1)[0][0]
-            if temp in banned:
-                del words_dict[temp]
+        see_once = {}
+        for i, ch in enumerate(s):
+            if ch in see_once:
+                see_once[ch] = -1
             else:
-                return temp
+                see_once[ch] = i
+
+        for v in see_once.values():
+            if v != -1:
+                return v
+                
+        return -1
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
-    banned = "hit"
+    s = "leetcode"
 
-    print(a.mostCommonWord(paragraph, banned))
+    print(a.firstUniqChar(s))
 
 
     
