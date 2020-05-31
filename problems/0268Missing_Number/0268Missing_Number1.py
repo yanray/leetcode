4a68d20832fe400b71f5b9c6abca125c83dede99
Yanrui Wang <yw2226@cornell.edu>
2020-05-31 16:54:24 +0800

    Update 0268Missing_Number1.py

--- problems/0268Missing_Number/0268Missing_Number1.py
+++ problems/0268Missing_Number/0268Missing_Number1.py
@@ -5,41 +5,23 @@ Author:  Yanrui
 date:    5/31/2020
 """
 
-class Logger:
+from typing import List
 
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.log_dict = {}
-
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
-        if message not in self.log_dict:
-            self.log_dict[message] = timestamp
-            return True
-        else:
-            if timestamp - self.log_dict[message] >= 10:
-                self.log_dict[message] = timestamp
-                return True
-            else:
-                return False
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        whole_num_set = set(range(len(nums) + 1))
+        miss_num_set = set(nums)
+        
+        return whole_num_set.difference(miss_num_set).pop()
 
 
 if __name__ == '__main__':
+    a = Solution()
 
-    logger = Logger()
+    nums = [9,6,4,2,3,5,7,0,1]
 
-    print(logger.shouldPrintMessage(1, "foo"))
-    print(logger.shouldPrintMessage(2, "bar"))
-    print(logger.shouldPrintMessage(3, "foo"))
-    print(logger.shouldPrintMessage(8, "bar"))
-    print(logger.shouldPrintMessage(10, "foo"))
-    print(logger.shouldPrintMessage(11, "foo"))
+    print(nums)
+    print(a.missingNumber(nums))
 
 
     
