07cb86d57128a9a769d5de11085d4b2826a1cbae
Yanrui Wang <yw2226@cornell.edu>
2020-06-09 17:49:56 +0800

    Update 0003Longest_Substring_Without_Repeating_Characters1.py

--- medium/0003Longest_Substring_Without_Repeating_Characters/0003Longest_Substring_Without_Repeating_Characters1.py
+++ medium/0003Longest_Substring_Without_Repeating_Characters/0003Longest_Substring_Without_Repeating_Characters1.py
@@ -2,25 +2,36 @@
 
 Version: 1.1 
 Author:  Yanrui 
-date:    5/31/2020
+date: 	 06/09/2020
 """
 
-import collections
-
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-    	return collections.Counter(s) == collections.Counter(t)
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        if not s:
+            return 0
 
+        sub_str_dict = {}
+        sub_len = [1]
+        sub_str_dict[s[0]] = 0
+
+        for i in range(1, len(s)):
+            if s[i] in sub_str_dict:
+                sub_len.append(min(i - sub_str_dict[s[i]], sub_len[i - 1] + 1))
+                sub_str_dict[s[i]] = i
+            else:
+                sub_str_dict[s[i]] = i
+                sub_len.append(sub_len[i - 1] + 1)
+
+        return max(sub_len)
 
 if __name__ == '__main__':
+ 
     a = Solution()
 
-    s = "anagram"
-    t = "nagaram"
+    s = "abba"
 
-    print(s)
-    print(t)
-    print(a.isAnagram(s, t))
+    print("input: ", s)
+    print("output: ", a.lengthOfLongestSubstring(s))
 
 
-    
