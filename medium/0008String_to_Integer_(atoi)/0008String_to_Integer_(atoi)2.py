52e7651ae0941892d63cc94547a12310ba0bd988
Yanrui Wang <yw2226@cornell.edu>
2020-06-09 15:24:04 +0800

    Update 0008String_to_Integer_(atoi)2.py

--- medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)2.py
+++ medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)2.py
@@ -11,40 +11,28 @@ class Solution:
     def myAtoi(self, str: str):
         
         str = str.lstrip()
-        
+
         if not str:
             return 0
-        
-        index = 0
+
         INT_MIN = -2 ** 31
         INT_MAX = 2 ** 31 - 1
-        
-        if str[index] == "-" and (index + 1) < len(str) and str[index + 1].isdigit():
-            new_str = str[index] + str[index + 1]
-            index += 2
-            while index < len(str) and str[index].isdigit():
-                new_str += str[index]
-                index += 1
 
-            return int(new_str) if int(new_str) >= INT_MIN else INT_MIN 
-        
-        elif str[index] == "+" and (index + 1) < len(str) and str[index + 1].isdigit():
-            new_str = str[index] + str[index + 1]
-            index += 2
-            while index < len(str) and str[index].isdigit():
-                new_str += str[index]
-                index += 1
+        if str[0] == "+" or str[0] == "-":
+            new_str = re.match("^\d+", str[1:])
+            if new_str == None:
+                return 0
+            else:
+                num = int(str[0] + new_str.group())
+                if num >= 0:
+                    return num if num <= INT_MAX else INT_MAX
+                else:
+                    return num if num >= INT_MIN else INT_MIN
 
-            return int(new_str) if int(new_str) <= INT_MAX else INT_MAX
+        if str[0].isdigit():
+            new_str = re.match("^\d+", str)
+            return int(new_str.group()) if int(new_str.group()) <= INT_MAX else INT_MAX
 
-        elif str[index].isdigit():
-            new_str = str[index]
-            index += 1
-            while index < len(str) and str[index].isdigit():
-                new_str += str[index]   
-                index += 1
-            return int(new_str) if int(new_str) <= INT_MAX else INT_MAX
-        
         else:
             return 0
 
