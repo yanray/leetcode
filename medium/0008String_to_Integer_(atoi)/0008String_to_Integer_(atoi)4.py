a11629d3043c8fba588376aabdef62db4b083f5a
Yanrui Wang <yw2226@cornell.edu>
2020-06-09 15:52:32 +0800

    Update 0008String_to_Integer_(atoi)4.py

--- medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)4.py
+++ medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)4.py
@@ -10,20 +10,12 @@ import re
 class Solution:
     def myAtoi(self, str: str):
         
-        def helper(x):
-            if not x: return 0
-            ret = 0
-            for i in x:
-                if not i.isdigit(): return ret
-                ret = ret *10 + int(i)
-            return ret
-
-        s = str.strip()
-        if not s: return 0
-        elif s[0] == '+': return min(2**31-1,helper(s[1:]))
-        elif s[0] == '-': return max(-2**31,-helper(s[1:]))
-        elif s[0].isdigit(): return min(2**31-1,helper(s))
-        else: return 0
+        match = re.match('[ ]*([+-]?\d+)', str)
+        if match:
+            i = int(match.group(1))            
+            return min(i, 2**31-1) if i >=0 else max(i, -2**31) 
+        else:
+            return 0
 
 
 
