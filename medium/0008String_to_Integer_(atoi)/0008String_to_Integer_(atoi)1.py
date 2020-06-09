67f4eec8b4ad5d414ea4e0ced31267ba1de40dba
Yanrui Wang <yw2226@cornell.edu>
2020-06-09 15:18:10 +0800

    Update 0008String_to_Integer_(atoi)1.py

--- medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)1.py
+++ medium/0008String_to_Integer_(atoi)/0008String_to_Integer_(atoi)1.py
@@ -5,44 +5,62 @@ Author:  Yanrui
 date:    06/08/2020
 """
 
+import re 
+
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
+    def myAtoi(self, str: str):
         
-        if numRows == 1:
-            return s
+        str = str.lstrip()
         
-        new_s = ""
+        if not str:
+            return 0
         
-        length_s = len(s)
         index = 0
-        curr_row = 1
-        compare_row = 1
-        for i in range(length_s):
-            new_s += s[index]
-            if curr_row == 1 or curr_row == numRows:
-                index += 2 * (numRows - curr_row) + 2 * (curr_row - 1)
-            # elif curr_row == numRows:
-                # index += 2 * (curr_row - 1)
-            else:
-                index += 2 * abs(compare_row - curr_row)
-                if compare_row == numRows:
-                    compare_row = 1
-                else:
-                    compare_row = numRows
-            if index >= length_s:
-                curr_row += 1
-                compare_row = numRows
-                index = curr_row - 1
-                
-        return new_s
+        INT_MIN = -2 ** 31
+        INT_MAX = 2 ** 31 - 1
+        
+        if str[index] == "-" and (index + 1) < len(str) and str[index + 1].isdigit():
+            new_str = str[index] + str[index + 1]
+            index += 2
+            while index < len(str) and str[index].isdigit():
+                new_str += str[index]
+                index += 1
+
+            return int(new_str) if int(new_str) >= INT_MIN else INT_MIN 
+        
+        elif str[index] == "+" and (index + 1) < len(str) and str[index + 1].isdigit():
+            new_str = str[index] + str[index + 1]
+            index += 2
+            while index < len(str) and str[index].isdigit():
+                new_str += str[index]
+                index += 1
+
+            return int(new_str) if int(new_str) <= INT_MAX else INT_MAX
+
+        elif str[index].isdigit():
+            new_str = str[index]
+            index += 1
+            while index < len(str) and str[index].isdigit():
+                new_str += str[index]   
+                index += 1
+            return int(new_str) if int(new_str) <= INT_MAX else INT_MAX
+        
+        else:
+            return 0
+
 
 
 if __name__ == '__main__':
+
     a = Solution()
 
-    s = "PAYPALISHIRING"
-    numRows = 3
+    s = "   -42"
+    print("input:", s)
+    print("output:", a.myAtoi(s))
 
-    print("old string", s)
-    print("new string", a.convert(s, numRows))
-    
+
+
+
+
+
+
