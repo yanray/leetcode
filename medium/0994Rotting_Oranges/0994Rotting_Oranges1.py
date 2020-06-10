08cbdfbf824641f09df3c6fdf13b86d7da75b0d1
Yanrui Wang <yw2226@cornell.edu>
2020-06-10 20:50:02 +0800

    Update 0994Rotting_Oranges1.py

--- medium/0994Rotting_Oranges/0994Rotting_Oranges1.py
+++ medium/0994Rotting_Oranges/0994Rotting_Oranges1.py
@@ -4,44 +4,61 @@ Version: 1.1
 Author:  Yanrui 
 date:    06/10/2020
 """
+from typing import List
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        """ convert s to st in the following manner to eliminate even odd conditin palindrom 
-        s = abc st = $#a#b#c#%"""
-        st = ''
-        for i in range(len(s)):
-            st += '#'+ s[i]
-        st = '$'+st+'#%'
+    def orangesRotting(self, grid: List[List[int]]) -> int:
         
-        P = [0]*len(st)
-        C,R = 0, 0
-        for i in range(1, len(st)-1):
-            mirr = 2*C-i
-            
-            # update already expanded palindrome
-            if i < R:
-                P[i] = min(R-i, P[mirr])
-            
-            while st[i+(1+P[i])] == st[i-(1+P[i])]:
-                P[i] += 1
-            
-            if i+P[i] > R:
-                C = i
-                R = i + P[i]
+        rotten_pos = []
+        num_fresh = 0
+        rotten_minutes = 0
         
-        # extract the longest palindromic substring from P, st
-        length = max(P)
-        index = P.index(length)
-        string = st[index-length:index+length]
-        return string.replace('#','')
+        num_r = len(grid)
+        num_c = len(grid[0])
+        for row in range(num_r):
+            for col in range(num_c):
+                if grid[row][col] == 2:
+                    rotten_pos.append((row, col))
+                if grid[row][col] == 1:
+                    num_fresh += 1
+        if num_fresh == 0:
+            return 0
+        
+        while rotten_pos and num_fresh != 0:
+            rotten_minutes += 1
+            copy = rotten_pos
+            rotten_pos = []
+            for (i, j) in copy:
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    rotten_pos.append((i - 1, j))
+                    grid[i - 1][j] = 2
+                    num_fresh -= 1
+                if i + 1 < num_r and grid[i + 1][j] == 1:
+                    rotten_pos.append((i + 1, j))
+                    grid[i + 1][j] = 2
+                    num_fresh -= 1
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    rotten_pos.append((i, j - 1))
+                    grid[i][j - 1] = 2
+                    num_fresh -= 1
+                if j + 1 < num_c and grid[i][j + 1] == 1:
+                    rotten_pos.append((i, j + 1))
+                    grid[i][j + 1] = 2
+                    num_fresh -= 1
+                
+        return rotten_minutes if num_fresh == 0 else -1
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    s = "babad"
+    grid = [[2,1,1],[1,1,0],[0,1,1]]
 
-    print("string: ", s)
-    print("output: ", a.longestPalindrome(s))
+    print("Input: ")
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end = "")
+        print()
+
+    print("Output: ", a.orangesRotting(grid))
     
