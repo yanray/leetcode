1d931c6a382e5bfd73d1ca06232b0e5971e00bc1
Yanrui Wang <yw2226@cornell.edu>
2020-06-10 11:09:20 +0800

    Update 0200Number_of_Islands1.py

--- medium/0200Number_of_Islands/0200Number_of_Islands1.py
+++ medium/0200Number_of_Islands/0200Number_of_Islands1.py
@@ -5,33 +5,52 @@ Author:  Yanrui
 date: 	 06/09/2020
 """
 
+from typing import List
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def numIslands(self, grid: List[List[str]]) -> int:
         
-        if not s:
-            return 0
-
-        sub_str_dict = {}
-        sub_len = [1]
-        sub_str_dict[s[0]] = 0
-
-        for i in range(1, len(s)):
-            if s[i] in sub_str_dict:
-                sub_len.append(min(i - sub_str_dict[s[i]], sub_len[i - 1] + 1))
-                sub_str_dict[s[i]] = i
-            else:
-                sub_str_dict[s[i]] = i
-                sub_len.append(sub_len[i - 1] + 1)
-
-        return max(sub_len)
+        if not grid:
+            return 0;
+        
+        visited = grid.copy() # in case we do not want to modify our input
+        islands = 0
+        stack = [] # this will simulate the call stack 
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if visited[i][j] != 'X' and grid[i][j] == '1':
+                    islands += 1
+                    stack.append((i, j))
+                    # perform dfs iteratively
+                    while stack:
+                        row, col = stack.pop()
+                        
+                        if grid[row][col] == '1' and visited[row][col] != 'X':
+                            visited[row][col] = 'X'
+                            
+                        if  row + 1 < len(grid) and grid[row + 1][col] == '1':
+                            stack.append((row + 1, col))
+                        if  col + 1 < len(grid[0]) and grid[row][col + 1] == '1':
+                            stack.append((row, col + 1))
+                        if row - 1 >= 0 and grid[row - 1][col] == '1':
+                            stack.append((row - 1, col))
+                        if col - 1 >= 0 and grid[row][col - 1] == '1':
+                            stack.append((row, col - 1))
+        return islands
 
 if __name__ == '__main__':
  
     a = Solution()
 
-    s = "abba"
+    grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
 
-    print("input: ", s)
-    print("output: ", a.lengthOfLongestSubstring(s))
+    print("Input: ")
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end = "")
+        print()
+
+    print("Output: ", a.numIslands(grid))
 
 
