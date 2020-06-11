1026966a1de4b66f9176b838a266b3cada030df4
Yanrui Wang <yw2226@cornell.edu>
2020-06-11 12:47:23 +0800

    Update 0122Best_Time_to_Buy_and_Sell_Stock_II1.py

--- problems/0122Best_Time_to_Buy_and_Sell_Stock_II/0122Best_Time_to_Buy_and_Sell_Stock_II1.py
+++ problems/0122Best_Time_to_Buy_and_Sell_Stock_II/0122Best_Time_to_Buy_and_Sell_Stock_II1.py
@@ -4,61 +4,34 @@ Version: 1.1
 Author:  Yanrui 
 date:    06/10/2020
 """
+
 from typing import List
 
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    def maxProfit(self, prices: List[int]) -> int:
+        # if prices == sorted(prices, reverse = True):
+        #     return 0
         
-        rotten_pos = []
-        num_fresh = 0
-        rotten_minutes = 0
-        
-        num_r = len(grid)
-        num_c = len(grid[0])
-        for row in range(num_r):
-            for col in range(num_c):
-                if grid[row][col] == 2:
-                    rotten_pos.append((row, col))
-                if grid[row][col] == 1:
-                    num_fresh += 1
-        if num_fresh == 0:
-            return 0
-        
-        while rotten_pos and num_fresh != 0:
-            rotten_minutes += 1
-            copy = rotten_pos
-            rotten_pos = []
-            for (i, j) in copy:
-                if i - 1 >= 0 and grid[i - 1][j] == 1:
-                    rotten_pos.append((i - 1, j))
-                    grid[i - 1][j] = 2
-                    num_fresh -= 1
-                if i + 1 < num_r and grid[i + 1][j] == 1:
-                    rotten_pos.append((i + 1, j))
-                    grid[i + 1][j] = 2
-                    num_fresh -= 1
-                if j - 1 >= 0 and grid[i][j - 1] == 1:
-                    rotten_pos.append((i, j - 1))
-                    grid[i][j - 1] = 2
-                    num_fresh -= 1
-                if j + 1 < num_c and grid[i][j + 1] == 1:
-                    rotten_pos.append((i, j + 1))
-                    grid[i][j + 1] = 2
-                    num_fresh -= 1
-                
-        return rotten_minutes if num_fresh == 0 else -1
+        valley = peak = 0
+        len_price = len(prices)
+        max_profit = 0
+        i = 0
+        while i < len_price - 1:
+            while i < len_price - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            valley = prices[i]
+
+            while i < len_price - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            peak = prices[i]
+            max_profit += peak - valley
+            
+        return max_profit
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    grid = [[2,1,1],[1,1,0],[0,1,1]]
-
-    print("Input: ")
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            print(grid[i][j], end = "")
-        print()
-
-    print("Output: ", a.orangesRotting(grid))
+    prices = [7,1,5,3,6,4]
+    print("max profit: ", a.maxProfit(prices))
     
