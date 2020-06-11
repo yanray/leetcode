8d0e39afb442a7351627051251fb03385f09c22e
Yanrui Wang <yw2226@cornell.edu>
2020-06-11 16:05:08 +0800

    Update 0349Intersection_of_Two_Arrays1.py

--- problems/0349Intersection_of_Two_Arrays/0349Intersection_of_Two_Arrays1.py
+++ problems/0349Intersection_of_Two_Arrays/0349Intersection_of_Two_Arrays1.py
@@ -5,47 +5,21 @@ Author:  Yanrui
 date:    06/11/2020
 """
 
-class MaxStack:
+from typing import List
 
-    def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.stack = []
-
-    def push(self, x: int) -> None:
-        self.stack.append(x)
-
-    def pop(self) -> int:
-        if len(self.stack) > 0:
-            return self.stack.pop()
-
-    def top(self) -> int:
-        if len(self.stack) > 0:
-            return self.stack[-1]
-
-    def peekMax(self) -> int:
-        if len(self.stack) > 0:
-            return max(self.stack)
-
-    def popMax(self) -> int:
-        if len(self.stack) > 0:
-            max_val = max(self.stack)
-            for i in range(len(self.stack) - 1, -1, -1):
-                if self.stack[i] == max_val:
-                    return self.stack.pop(i)
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
 
 
 if __name__ == '__main__':
 
-    stack = MaxStack()
-    stack.push(5)
-    stack.push(1)
-    stack.push(5)
-    stack.top()
-    stack.popMax()
-    stack.top()
-    stack.peekMax()
-    stack.pop()
-    stack.top()
-    
+    a = Solution()
+
+    nums1 = [1,2,2,1]
+    nums2 = [2,2]
+
+    print("nums1:", nums1)
+    print("nums2:", nums2)
+
+    print(a.intersection(nums1, nums2))
