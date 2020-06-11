5d281f14b5babd6709854a227444f99ab0d0243c
Yanrui Wang <yw2226@cornell.edu>
2020-06-11 12:47:25 +0800

    Create 0122Best_Time_to_Buy_and_Sell_Stock_II2.py

---
+++ problems/0122Best_Time_to_Buy_and_Sell_Stock_II/0122Best_Time_to_Buy_and_Sell_Stock_II2.py
@@ -0,0 +1,25 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date:    06/10/2020
+"""
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0 
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+                
+        return max_profit 
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    prices = [7,1,5,3,6,4]
+    print("max profit: ", a.maxProfit(prices))
+    
