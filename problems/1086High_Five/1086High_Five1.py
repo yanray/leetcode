03002e5a0999453cb2a6d736ecea5240913f236a
Yanrui Wang <yw2226@cornell.edu>
2020-06-12 17:27:49 +0800

    Create 1086High_Five1.py

---
+++ problems/1086High_Five/1086High_Five1.py
@@ -0,0 +1,20 @@
+"""
+
+Version: 1.1 
+Author:  Yanrui 
+date: 	 06/12/2020
+"""
+
+from typing import List
+
+
+if __name__ == '__main__':
+
+	a = Solution()
+
+	nums = [1,2,3,4]
+
+	print("Input: ", nums)
+	print("Output: ", a.productExceptSelf(nums))
+
+
