06d9e808b3b9e7fb4c9b730c68308b472bb9edd2
Yanrui Wang <yw2226@cornell.edu>
2020-06-13 00:16:28 +0800

    Update 1086High_Five3.py

--- problems/1086High_Five/1086High_Five3.py
+++ problems/1086High_Five/1086High_Five3.py
@@ -10,29 +10,10 @@ import numpy as np
 
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        # use collectioni for hashmap
-        import collections
-        # use heapq to get only top 5 values
-        import heapq
-
-        d = collections.defaultdict(list)
-        # print(d)
-
-        for idx, val in items:
-            # adding values for each id in hashmap
-            heapq.heappush(d[idx], val)
-            # print(idx, val, d)
-
-            if len(d[idx]) > 5:
-                # print("if")
-                # heappop pops the smallest value
-                # so we alway has the length 5 for each id
-                heapq.heappop(d[idx])
-                # print(d)
-
-        # print([[i, sum(d[i]) // 5] for i in sorted(d)])
-        ans = [[i, sum(d[i]) // 5] for i in sorted(d)]
-        return ans
+        D = collections.defaultdict(list)
+        for student, score in items:
+            bisect.insort(D[student], score) # insert in a list in increasing order.
+        return [[student, sum(D[student][-5:])//5] for student in D]
 
 if __name__ == '__main__':
 
