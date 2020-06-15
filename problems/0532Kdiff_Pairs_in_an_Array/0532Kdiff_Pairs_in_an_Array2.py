cd0dc1627337cc14580ca8f398cbe101a4d41cdf
Yanrui Wang <yw2226@cornell.edu>
2020-06-15 13:01:45 +0800

    Update 0532Kdiff_Pairs_in_an_Array2.py

--- problems/0532Kdiff_Pairs_in_an_Array/0532Kdiff_Pairs_in_an_Array2.py
+++ problems/0532Kdiff_Pairs_in_an_Array/0532Kdiff_Pairs_in_an_Array2.py
@@ -6,37 +6,38 @@ date: 	 06/12/2020
 """
 
 from typing import List
-import collections
 
 class Solution:
-    def findPairs(self, nums: List[int], k: int) -> int:
+    def findPairs(self, nums: List[int], k: int) -> int: 
+        
         if not nums or k < 0:
             return 0
         
-        output = 0
-        if k == 0:
-            for v in collections.Counter(nums).values():
-                if v >= 2:
-                    output += 1
-            return output
-        
         nums.sort()
-        
-        hash_dict = {}
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                continue
-            if nums[i] in hash_dict:
-                output += 1
-            if nums[i] + k <= nums[-1]:
-                hash_dict[nums[i] + k] = i
-        if nums[-1] in hash_dict:
-                output += 1
-        
-                
-        # print(nums)
-        # print(hash_dict)
-        return output
+
+        N = len(nums)
+
+        i = pairs = 0
+        j = 1
+
+        while j < N:
+            if j < N - 1 and nums[j] == nums[j + 1]:
+                j += 1
+
+            elif nums[j] == nums[i] + k:
+                pairs += 1
+                i += 1
+                j += 1
+
+            elif nums[j] > nums[i] + k:
+                i += 1
+
+            elif nums[j] < nums[i] + k:
+                j += 1
+
+            j = max(j, i + 1)
+
+        return pairs
 
 if __name__ == '__main__':
 
