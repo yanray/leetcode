62106c8f0f88bc020d682a4d5aae3d212b70ee9b
Yanrui Wang <yw2226@cornell.edu>
2020-06-17 22:20:54 +0800

    Update 0256Paint_House1.py

--- problems/0256Paint_House/0256Paint_House1.py
+++ problems/0256Paint_House/0256Paint_House1.py
@@ -3,21 +3,32 @@ leetcode 0001 TwoSum
 
 Version: 1.1 
 Author:  Yanrui 
-date: 	 5/20/2020
+date: 	 06/17/2020
 """
 
+from typing import List
 
 class Solution:
-    def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in nums:
-                if i != nums.index(diff):
-                    return [i, nums.index(diff)]
+    def minCost(self, costs: List[List[int]]) -> int:
+        
+        if not costs:
+            return 0
+        
+        for i in range(1, len(costs)):
+            costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
+            costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
+            costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
+        
+        return min(costs[-1])
 
 
 if __name__ == '__main__':
-	s = Solution()
-	nums = [2, 7, 11, 15]
-	target = 9
-	print(s.twoSum(nums, target))
+    a = Solution()
+
+    costs = [[17,2,17],[16,16,5],[14,3,19]]
+    print("input: ", costs)
+    print("output: ", a.minCost(costs))
+
+
+
+
