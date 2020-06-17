db0dc0243b465e22394acff9e2f1f0710cece248
Yanrui Wang <yw2226@cornell.edu>
2020-06-18 00:11:19 +0800

    Update 0344Reverse_String1.py

--- problems/0344Reverse_String/0344Reverse_String1.py
+++ problems/0344Reverse_String/0344Reverse_String1.py
@@ -9,25 +9,22 @@ date: 	 06/17/2020
 from typing import List
 
 class Solution:
-    def minCost(self, costs: List[List[int]]) -> int:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
         
-        if not costs:
-            return 0
-        
-        for i in range(1, len(costs)):
-            costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
-            costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
-            costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
-        
-        return min(costs[-1])
+        for i in range(len(s) // 2):
+            s[i], s[~i] = s[~i], s[i]
 
 
 if __name__ == '__main__':
     a = Solution()
 
-    costs = [[17,2,17],[16,16,5],[14,3,19]]
-    print("input: ", costs)
-    print("output: ", a.minCost(costs))
+    s = ["h","e","l","l","o"]
+    print("input: ", s)
+    a.reverseString(s)
+    print("reversed s: ", s)
 
 
 
