f4ae6c4dadc8987d947888472a5d3488b9b11c00
Yanrui Wang <yw2226@cornell.edu>
2020-06-17 23:56:00 +0800

    Create 0256Paint_House4.py

---
+++ problems/0256Paint_House/0256Paint_House4.py
@@ -0,0 +1,49 @@
+"""
+leetcode 0001 TwoSum
+
+Version: 1.1 
+Author:  Yanrui 
+date: 	 06/17/2020
+"""
+
+from typing import List
+
+class Solution:
+    def minCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+
+        def paint_cost(n, color):
+            if (n, color) in self.memo:
+                return self.memo[(n, color)]
+            total_cost = costs[n][color]
+            if n == len(costs) - 1:
+                pass
+            elif color == 0:
+                total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
+            elif color == 1:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
+            else:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
+            self.memo[(n, color)] = total_cost
+            return total_cost
+
+        if costs == []:
+            return 0
+
+        self.memo = {}
+        return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
+
+
+if __name__ == '__main__':
+    a = Solution()
+
+    costs = [[17,2,17],[16,16,5],[14,3,19]]
+    print("input: ", costs)
+    print("output: ", a.minCost(costs))
+
+
+
+
