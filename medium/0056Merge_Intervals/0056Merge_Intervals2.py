88f4eadc3c316f407612a720a3dc27d6e177214f
Yanrui Wang <yw2226@cornell.edu>
2020-06-18 19:35:13 +0800

    Update 0056Merge_Intervals2.py

--- medium/0056Merge_Intervals/0056Merge_Intervals2.py
+++ medium/0056Merge_Intervals/0056Merge_Intervals2.py
@@ -9,25 +9,21 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
-        if not intervals:
-            return intervals
-        
-        intervals = sorted(intervals, key = lambda x : x[0])
-        output = []
-        left, right = intervals[0][0], intervals[0][1]
-        for i in range(1, len(intervals)):
-            if right >= intervals[i][1]:
-                continue
-            if right >= intervals[i][0]:
-                right = intervals[i][1]
-            else:
-                output.append([left, right])
-                left, right = intervals[i][0], intervals[i][1]
-        output.append([left, right])
-            
-        return output
 
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
 
 if __name__ == '__main__':
 
