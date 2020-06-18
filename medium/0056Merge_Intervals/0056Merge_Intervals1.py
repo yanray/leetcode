62f2f37f06a6708b9824273687ede03db2808e9d
Yanrui Wang <yw2226@cornell.edu>
2020-06-18 19:07:53 +0800

    Update 0056Merge_Intervals1.py

--- medium/0056Merge_Intervals/0056Merge_Intervals1.py
+++ medium/0056Merge_Intervals/0056Merge_Intervals1.py
@@ -2,47 +2,39 @@
 
 Version: 1.1 
 Author:  Yanrui 
-date:    06/17/2020
+date:    06/18/2020
 """
 
 from typing import List
 
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        def helper(image, i, j, original_color, new_color):
-            
-            nr = len(image)
-            nc = len(image[0])
-            
-            if i < 0 or i >= nr or j < 0 or j >= nc or image[i][j] != original_color:
-                return 
-            
-            image[i][j] = new_color
-            helper(image, i - 1, j, original_color, new_color)
-            helper(image, i + 1, j, original_color, new_color)
-            helper(image, i, j - 1, original_color, new_color)
-            helper(image, i, j + 1, original_color, new_color)
-
-        if image[sr][sc] == newColor:
-            return image
-    
-        new_image = image.copy()
-        # new_image = image
-        helper(new_image, sr, sc, image[sr][sc], newColor)
+        if not intervals:
+            return intervals
         
-        return new_image
+        intervals = sorted(intervals, key = lambda x : x[0])
+        output = []
+        left, right = intervals[0][0], intervals[0][1]
+        for i in range(1, len(intervals)):
+            if right >= intervals[i][1]:
+                continue
+            if right >= intervals[i][0]:
+                right = intervals[i][1]
+            else:
+                output.append([left, right])
+                left, right = intervals[i][0], intervals[i][1]
+        output.append([left, right])
+            
+        return output
 
 
 if __name__ == '__main__':
 
     a = Solution()
 
-    image = [[1,1,1],[1,1,0],[1,0,1]]
-    sr = 1 
-    sc = 1
-    newColor = 2
-    print("Input: ", image)
-    print("Output: ", a.floodFill(image, sr, sc, newColor))
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    print("Input: ", intervals)
+    print("Output: ", a.merge(intervals))
 
 
