16a54912496c86fe4bbe7a67e3aba6c1a9d5ce28
Yanrui Wang <yw2226@cornell.edu>
2020-06-18 10:36:53 +0800

    Update 0773Flood_Fill1.py

--- problems/0773Flood_Fill/0773Flood_Fill1.py
+++ problems/0773Flood_Fill/0773Flood_Fill1.py
@@ -2,24 +2,47 @@
 
 Version: 1.1 
 Author:  Yanrui 
-date:    06/11/2020
+date:    06/17/2020
 """
 
 from typing import List
 
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        def helper(image, i, j, original_color, new_color):
+            
+            nr = len(image)
+            nc = len(image[0])
+            
+            if i < 0 or i >= nr or j < 0 or j >= nc or image[i][j] != original_color:
+                return 
+            
+            image[i][j] = new_color
+            helper(image, i - 1, j, original_color, new_color)
+            helper(image, i + 1, j, original_color, new_color)
+            helper(image, i, j - 1, original_color, new_color)
+            helper(image, i, j + 1, original_color, new_color)
+
+        if image[sr][sc] == newColor:
+            return image
+    
+        new_image = image.copy()
+        # new_image = image
+        helper(new_image, sr, sc, image[sr][sc], newColor)
+        
+        return new_image
 
 
 if __name__ == '__main__':
 
     a = Solution()
 
-    nums1 = [1,2,2,1]
-    nums2 = [2,2]
+    image = [[1,1,1],[1,1,0],[1,0,1]]
+    sr = 1 
+    sc = 1
+    newColor = 2
+    print("Input: ", image)
+    print("Output: ", a.floodFill(image, sr, sc, newColor))
 
-    print("nums1:", nums1)
-    print("nums2:", nums2)
 
-    print(a.intersection(nums1, nums2))
